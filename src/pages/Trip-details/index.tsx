04101445f13d01d35d7a346fe4dd4ac53ed6c50e
Emmanuel Marcos de  Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-07-11 09:56:12 -0300

    Dev (#5)
    * style: I created the stylization of the activity registration page
    
    * chore: activity registration modal completed
    
    * chore: created the activity page components and installed the tailwind variants dependency
    
    * update

--- src/pages/Trip-details/index.tsx
+++ src/pages/Trip-details/index.tsx
@@ -13,6 +13,7 @@ export function TripDetailsPage() {
   function openCreateActivityModal() {
     setIsCreateActivityModalOpen(true)
   }
+
   function closeCreateActivityModal() {
     setIsCreateActivityModalOpen(false)
   }
@@ -20,26 +21,35 @@ export function TripDetailsPage() {
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <DestinationAndDateHeader />
+
+
       <main className="flex gap-16 px-4">
-        <div className="flex-1 sapce-y-6">
+        <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
+
+
             <button
               onClick={openCreateActivityModal}
-              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium  flex items-center gap-2 hover:bg-lime-400"       
-              <Plus className="size-5 text-bg-lime-950 " />
-              Cadastrar Atividade
+              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+            >
+              <Plus className="size-5" />
+              Cadastrar atividade
             </button>
           </div>
-        <Activivies />
+
+          <Activivies />
+
         </div>
 
         <div className="w-80 space-y-6">
           <ImportantLinks />
+
+
           <div className="w-full h-px bg-zinc-800" />
-          <div>
-            <Guests />
-          </div>
+
+          <Guests />
+
         </div>
       </main>
 
@@ -47,7 +57,9 @@ export function TripDetailsPage() {
         <CreateActivityModal
           closeCreateActivityModal={closeCreateActivityModal}
         />
-      )}   
+
+      )}
+
     </div>
   )
 }
